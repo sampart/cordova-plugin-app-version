76b560fa5b6ad703206058d48c654c26ac98ee5e
Robert (Jamie) Munro <rjmunro@arjam.net>
2013-07-11 11:19:51 +0100

    Add plugin.xml and convert JS into module

--- www/AppVersionPlugin.js
+++ www/AppVersionPlugin.js
@@ -1,14 +1,14 @@
-(function (cordova) {
-  // Returns a jQuery deferred object, or pass a success and fail callbacks if you don't want to use jQuery
-  cordova.getAppVersion = function (success, fail) {
-    var dfr = null;
-    if ((typeof success) === 'undefined' && jQuery) {
-      dfr = jQuery.Deferred();
-      success = dfr.resolve;
-      fail = dfr.reject;
-    }
-    // 5th param is NOT optional. must be at least empty array
-    cordova.exec(success, fail, "AppVersion", "getVersionNumber", []);
-    return dfr;
-  };
-}(cordova));
+// Returns a jQuery deferred object, or pass a success and fail callbacks if you don't want to use jQuery
+var getAppVersion = function (success, fail) {
+  var dfr = null;
+  if ((typeof success) === 'undefined' && jQuery) {
+    dfr = jQuery.Deferred();
+    success = dfr.resolve;
+    fail = dfr.reject;
+  }
+  // 5th param is NOT optional. must be at least empty array
+  cordova.exec(success, fail, "AppVersion", "getVersionNumber", []);
+  return dfr;
+};
+
+module.exports = getAppVersion;
