c983e794625e28c5295f5f8e7cac0d43103851f2
Gediminas Šaltenis <gediminas@saltenis.lt>
2014-07-22 18:29:03 +0300

    fixing tab to space

--- src/wp8/AppVersion.cs
+++ src/wp8/AppVersion.cs
@@ -10,15 +10,15 @@ using System.Xml.Linq;
 
 namespace Cordova.Extension.Commands
 {
-	public class AppVersion : BaseCommand
-	{
-		public void getVersionNumber(string empty)
+    public class AppVersion : BaseCommand
+    {
+        public void getVersionNumber(string empty)
         {
-			//Windows.ApplicationModel.Package.current.id.version is NOT working in Windows Phone 8
-			//Workaround based on http://stackoverflow.com/questions/14371275/how-can-i-get-my-windows-store-apps-title-and-version-info
+            //Windows.ApplicationModel.Package.current.id.version is NOT working in Windows Phone 8
+            //Workaround based on http://stackoverflow.com/questions/14371275/how-can-i-get-my-windows-store-apps-title-and-version-info
             String version= XDocument.Load("WMAppManifest.xml").Root.Element("App").Attribute("Version").Value;
 
             this.DispatchCommandResult(new PluginResult(PluginResult.Status.OK, version));
-		}
-	}
+        }
+    }
 }
