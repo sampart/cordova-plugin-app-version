eb17c49206d5da481b973f92bb26748dc75baf7d
Oliver Salzburg <oliver.salzburg@gmail.com>
2014-12-01 16:50:39 +0100

    Properly invoke provided callbacks in proxy

--- www/blackberry10/AppVersionProxy.js
+++ www/blackberry10/AppVersionProxy.js
@@ -1,5 +1,16 @@
 module.exports = {
-    getVersionNumber: function() {
+    getVersionNumber: function( success, fail ) {
+        if( !blackberry || !blackberry.app || !blackberry.app.version ) {
+            if( fail ) {
+                return fail();
+            } else {
+                return "";
+            }
+        }
+
+        if( success ) {
+            return success( blackberry.app.version );
+        }
         return blackberry.app.version;
     }
 };
