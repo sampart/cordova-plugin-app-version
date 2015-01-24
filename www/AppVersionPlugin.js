83a2e6c191ba9eab17686b9b87eb5976f9d9a30a
Li <uranus1986.cl@gmail.com>
2015-01-24 18:09:22 +0800

    Added phonegap interface.

--- www/AppVersionPlugin.js
+++ www/AppVersionPlugin.js
@@ -1,24 +1,47 @@
 // Returns a jQuery or AngularJS deferred object, or pass a success and fail callbacks if you don't want to use jQuery or AngularJS
-var getAppVersion = function (success, fail) {
-  var toReturn, deferred;
-  if ((typeof success) === 'undefined') {
-    if(window.jQuery){
-      deferred = jQuery.Deferred();
-      toReturn = deferred;
-    } else if(window.angular){
-      var injector = angular.injector(["ng"]);
-      var $q = injector.get("$q");
-      deferred = $q.defer();
-      toReturn = deferred.promise
-    } else {
-      return console.error('AppVersion either needs a success callback, or jQuery/AngularJS defined for using promises');
+var getAppVersion = {
+  getVersionNumber: function (success, fail) {
+    var toReturn, deferred;
+    if ((typeof success) === 'undefined') {
+      if(window.jQuery){
+        deferred = jQuery.Deferred();
+        toReturn = deferred;
+      } else if(window.angular){
+        var injector = angular.injector(["ng"]);
+        var $q = injector.get("$q");
+        deferred = $q.defer();
+        toReturn = deferred.promise
+      } else {
+        return console.error('AppVersion either needs a success callback, or jQuery/AngularJS defined for using promises');
+      }
+      success = deferred.resolve;
+      fail = deferred.reject;
     }
-    success = deferred.resolve;
-    fail = deferred.reject;
+    // 5th param is NOT optional. must be at least empty array
+    cordova.exec(success, fail, "AppVersion", "getVersionNumber", []);
+    return toReturn;
+  },
+  getVersionCode: function (success, fail) {
+    var toReturn, deferred;
+    if ((typeof success) === 'undefined') {
+      if(window.jQuery){
+        deferred = jQuery.Deferred();
+        toReturn = deferred;
+      } else if(window.angular){
+        var injector = angular.injector(["ng"]);
+        var $q = injector.get("$q");
+        deferred = $q.defer();
+        toReturn = deferred.promise
+      } else {
+        return console.error('AppVersion either needs a success callback, or jQuery/AngularJS defined for using promises');
+      }
+      success = deferred.resolve;
+      fail = deferred.reject;
+    }
+    // 5th param is NOT optional. must be at least empty array
+    cordova.exec(success, fail, "AppVersion", "getVersionCode", []);
+    return toReturn;
   }
-  // 5th param is NOT optional. must be at least empty array
-  cordova.exec(success, fail, "AppVersion", "getVersionNumber", []);
-  return toReturn;
 };
 
 module.exports = getAppVersion;
