697c2cbd3d60469b3cd0ecac32bd6379cc133dd4
Robert (Jamie) Munro <rjmunro@arjam.net>
2015-05-20 16:53:23 +0100

    Fix indentation for consistency (tabs -> 2 spaces)
    Also removed some whitespace from line ends.

--- src/android/AppVersion.java
+++ src/android/AppVersion.java
@@ -11,35 +11,35 @@ import android.content.pm.PackageManager.NameNotFoundException;
 import android.content.pm.PackageManager;
 
 public class AppVersion extends CordovaPlugin {
-	@Override
-        public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
+  @Override
+  public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
 
-		try {	
-			if (action.equals("getAppName")) {
-				PackageManager packageManager = this.cordova.getActivity().getPackageManager();
-				ApplicationInfo app = packageManager.getApplicationInfo(this.cordova.getActivity().getPackageName(), 0);
-				callbackContext.success((String)packageManager.getApplicationLabel(app));
-				return true;
-			}
-			if (action.equals("getPackageName")) {
-				callbackContext.success(this.cordova.getActivity().getPackageName());
-				return true;
-			}
-			if (action.equals("getVersionNumber")) {
-				PackageManager packageManager = this.cordova.getActivity().getPackageManager();
-				callbackContext.success(packageManager.getPackageInfo(this.cordova.getActivity().getPackageName(), 0).versionName);
-			return true;
-			}
-			if (action.equals("getVersionCode")) {
-				PackageManager packageManager = this.cordova.getActivity().getPackageManager();
-				callbackContext.success(packageManager.getPackageInfo(this.cordova.getActivity().getPackageName(), 0).versionCode);
-			return true;
-			}
-			return false;
-		} catch (NameNotFoundException e) {
-			callbackContext.success("N/A");
-			return true;
-		}
-	}
+    try {
+      if (action.equals("getAppName")) {
+        PackageManager packageManager = this.cordova.getActivity().getPackageManager();
+        ApplicationInfo app = packageManager.getApplicationInfo(this.cordova.getActivity().getPackageName(), 0);
+        callbackContext.success((String)packageManager.getApplicationLabel(app));
+        return true;
+      }
+      if (action.equals("getPackageName")) {
+        callbackContext.success(this.cordova.getActivity().getPackageName());
+        return true;
+      }
+      if (action.equals("getVersionNumber")) {
+        PackageManager packageManager = this.cordova.getActivity().getPackageManager();
+        callbackContext.success(packageManager.getPackageInfo(this.cordova.getActivity().getPackageName(), 0).versionName);
+      return true;
+      }
+      if (action.equals("getVersionCode")) {
+        PackageManager packageManager = this.cordova.getActivity().getPackageManager();
+        callbackContext.success(packageManager.getPackageInfo(this.cordova.getActivity().getPackageName(), 0).versionCode);
+      return true;
+      }
+      return false;
+    } catch (NameNotFoundException e) {
+      callbackContext.success("N/A");
+      return true;
+    }
+  }
 
 }
