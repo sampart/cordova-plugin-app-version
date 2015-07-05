c3f898d627b1391a61a004206873cfc7b90e4517
Jeff Kirkell <jeff.kirkell@gmail.com>
2015-07-05 12:24:56 -0400

    wrapped IOS methods to run in background
    Wrapped the IOS methods inside the AppVersion.m to run in background to resolve XCode warning.

--- src/ios/AppVersion.m
+++ src/ios/AppVersion.m
@@ -3,19 +3,53 @@
 
 @implementation AppVersion
 
+- (void)getAppName : (CDVInvokedUrlCommand *)command
+{
+    [self.commandDelegate runInBackground:^{
+        NSString * callbackId = command.callbackId;
+        NSString * version =[[[NSBundle mainBundle]infoDictionary]objectForKey :@"CFBundleDisplayName"];
+        CDVPluginResult * pluginResult =[CDVPluginResult resultWithStatus : CDVCommandStatus_OK messageAsString : version];
+        [self.commandDelegate sendPluginResult : pluginResult callbackId : callbackId];
+    }];
+}
+
+- (void)getPackageName:(CDVInvokedUrlCommand*)command
+{
+    [self.commandDelegate runInBackground:^{
+        NSString* callbackId = command.callbackId;
+        NSString* packageName = [[[NSBundle mainBundle] infoDictionary] objectForKey:@"CFBundleIdentifier"];
+        CDVPluginResult* pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:packageName];
+        [self.commandDelegate sendPluginResult:pluginResult callbackId:callbackId];
+    }];
+}
+
 - (void)getVersionNumber:(CDVInvokedUrlCommand*)command
 {
+    [self.commandDelegate runInBackground:^{
+        NSString* callbackId = command.callbackId;
+        NSString* version = [[[NSBundle mainBundle] infoDictionary] objectForKey:@"CFBundleShortVersionString"];
+        if (version == nil) {
+            NSLog(@"CFBundleShortVersionString was nil, attempting CFBundleVersion");
+            version = [[[NSBundle mainBundle] infoDictionary] objectForKey:@"CFBundleVersion"];
+            if (version == nil) {
+                NSLog(@"CFBundleVersion was also nil, giving up");
+                // not calling error callback here to maintain backward compatibility
+            }
+        }
 
-    NSString* callbackId = command.callbackId;
-    NSString* version = [[[NSBundle mainBundle] infoDictionary] objectForKey:@"CFBundleVersion"];
+        CDVPluginResult* pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:version];
+        [self.commandDelegate sendPluginResult:pluginResult callbackId:callbackId];
+    }];
+}
 
-    CDVPluginResult* pluginResult = nil;
-    NSString* javaScript = nil;
-
-    pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:version];
-    javaScript = [pluginResult toSuccessCallbackString:callbackId];
-
-    [self writeJavascript:javaScript];
+- (void)getVersionCode:(CDVInvokedUrlCommand*)command
+{
+    [self.commandDelegate runInBackground:^{
+        NSString* callbackId = command.callbackId;
+        NSString* version = [[[NSBundle mainBundle] infoDictionary] objectForKey:@"CFBundleVersion"];
+        CDVPluginResult* pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:version];
+        [self.commandDelegate sendPluginResult:pluginResult callbackId:callbackId];
+    }];
 }
 
 @end
